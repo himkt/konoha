4f1709f405f6f513b7a7df701e07eeb11d4423bd
himkt <himkt@klis.tsukuba.ac.jp>
2018-08-22 13:56:19 +0000

    add word tokenizer

---
+++ src/word_tokenizer.py
@@ -0,0 +1,31 @@
+
+
+class WordTokenizer:
+    def __init__(self, tokenizer='KyTea'):
+        if tokenizer == 'KyTea':
+            import Mykytea
+            self.tokenizer = Mykytea.Mykytea('')
+            self.tokenize = self._kytea_tokenize
+
+        elif tokenizer == 'MeCab':
+            import natto
+            self.tokenizer = natto.MeCab('-Owakati')
+            self.tokenize = self._mecab_tokenize
+
+        else:
+            raise Exception('Bad')
+
+    def _mecab_tokenize(self, sentence):
+        return self.tokenizer.parse(sentence)
+
+    def _kytea_tokenize(self, sentence):
+        return ' '.join(self.tokenizer.getWS(sentence))
+
+
+if __name__ == '__main__':
+    word_tokenizer = WordTokenizer('KyTea')
+    res = word_tokenizer.tokenize('我輩は猫である')
+    print(res)
+    word_tokenizer = WordTokenizer('MeCab')
+    res = word_tokenizer.tokenize('我輩は猫である')
+    print(res)
---
+++ tests/test_word_tokenize.py
@@ -0,0 +1,19 @@
+from src.word_tokenizer import WordTokenizer
+import unittest
+
+
+SENTENCE1 = '吾輩は猫である'
+
+
+class TestWordTokenizer(unittest.TestCase):
+    def test_word_tokenize_with_kytea(self):
+        tokenizer = WordTokenizer('KyTea')
+        expect = '吾輩 は 猫 で あ る'
+        result = tokenizer.tokenize(SENTENCE1)
+        self.assertEqual(expect, result)
+
+    def test_word_tokenize_with_mecab(self):
+        tokenizer = WordTokenizer('MeCab')
+        expect = '吾輩 は 猫 で ある'
+        result = tokenizer.tokenize(SENTENCE1)
+        self.assertEqual(expect, result)
