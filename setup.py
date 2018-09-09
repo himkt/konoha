36482326e09c1f816597381eca659277ab4aab03
makoto-hiramatsu <makoto-hiramatsu@cookpad.com>
2018-09-09 13:49:31 +0900

    bump up the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 
 setup(name='tiny_tokenizer',
-      version='1.0',
+      version='1.0.1',
       description='Tiny Word/Sentence Tokenizer',
       author='himkt',
       author_email='himkt@klis.tsukuba.ac.jp',
