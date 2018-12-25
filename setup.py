fd1b1a9d21b233c26401ae75b8166dd1d3f339d8
himkt <himkt@klis.tsukuba.ac.jp>
2018-12-25 14:32:42 +0900

    Bump up the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 
 setup(name='tiny_tokenizer',
-      version='1.0.3',
+      version='1.0.4',
       description='Tiny Word/Sentence Tokenizer',
       author='himkt',
       author_email='himkt@klis.tsukuba.ac.jp',
