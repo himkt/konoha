cde1eaa373495b9c7f30349152050c58276e45dc
himkt <himkt@cookpad.com>
2019-07-09 14:48:34 +0900

    Apply black

--- example/tokenize_demo.py
+++ example/tokenize_demo.py
@@ -2,26 +2,26 @@ from tiny_tokenizer import SentenceTokenizer
 from tiny_tokenizer import WordTokenizer
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sentence_tokenizer = SentenceTokenizer()
     word_tokenizers = []
     word_tokenizers.append(WordTokenizer())  # return input directly
-    word_tokenizers.append(WordTokenizer('MeCab'))
-    word_tokenizers.append(WordTokenizer('KyTea'))
-    word_tokenizers.append(WordTokenizer('Sentencepiece', 'data/model.spm'))
-    word_tokenizers.append(WordTokenizer('Character'))
-    print('Finish creating word tokenizers')
+    word_tokenizers.append(WordTokenizer("MeCab"))
+    word_tokenizers.append(WordTokenizer("KyTea"))
+    word_tokenizers.append(WordTokenizer("Sentencepiece", "data/model.spm"))
+    word_tokenizers.append(WordTokenizer("Character"))
+    print("Finish creating word tokenizers")
     print()
 
-    document = '我輩は猫である。名前はまだない'
-    print(f'Given document: {document}')
+    document = "我輩は猫である。名前はまだない"
+    print(f"Given document: {document}")
 
     sentences = sentence_tokenizer.tokenize(document)
     for sentence_id, sentence in enumerate(sentences):
-        print(f'#{sentence_id}: {sentence}')
+        print(f"#{sentence_id}: {sentence}")
 
         for tokenizer in word_tokenizers:
             result = tokenizer.tokenize(sentence)
-            print(f'Tokenizer ({tokenizer.tokenizer_name}): {result}')
+            print(f"Tokenizer ({tokenizer.tokenizer_name}): {result}")
 
         print()
--- tests/test_sentence_tokenize.py
+++ tests/test_sentence_tokenize.py
@@ -2,50 +2,50 @@ from tiny_tokenizer.sentence_tokenizer import SentenceTokenizer
 import unittest
 
 
-DOCUMENT1 = '''
+DOCUMENT1 = """
 私は猫である。にゃお。\r\n
 にゃにゃ
 
 わんわん。にゃーにゃー。
-'''
+"""
 
-DOCUMENT2 = '''
+DOCUMENT2 = """
 私は猫である（ただしかわいいものとする。異議は認める）。にゃお。\r\n
 にゃにゃ
-'''
+"""
 
-DOCUMENT3 = '''
+DOCUMENT3 = """
 猫「にゃおにゃ。ただしかわいいものとする。異議は認める」。
 
 にゃお。にゃにゃ
-'''
+"""
 
-DOCUMENT4 = '''
+DOCUMENT4 = """
 わんわん。「にゃ？」(にゃー）わんわん。「わおーん。」（犬より。）
-'''
+"""
 
 
 class TestSentenceTokenizer(unittest.TestCase):
     def test_sentence_tokenize(self):
         corpus = SentenceTokenizer()
-        expect = ['私は猫である。', 'にゃお。', 'にゃにゃ', 'わんわん。', 'にゃーにゃー。']
+        expect = ["私は猫である。", "にゃお。", "にゃにゃ", "わんわん。", "にゃーにゃー。"]
         result = corpus.tokenize(DOCUMENT1)
         self.assertEqual(expect, result)
 
     def test_sentence_tokenize_with_bracket(self):
         corpus = SentenceTokenizer()
-        expect = ['私は猫である（ただしかわいいものとする。異議は認める）。', 'にゃお。', 'にゃにゃ']
+        expect = ["私は猫である（ただしかわいいものとする。異議は認める）。", "にゃお。", "にゃにゃ"]
         result = corpus.tokenize(DOCUMENT2)
         self.assertEqual(expect, result)
 
     def test_sentence_tokenize_with_quotation(self):
         corpus = SentenceTokenizer()
-        expect = ['猫「にゃおにゃ。ただしかわいいものとする。異議は認める」。', 'にゃお。', 'にゃにゃ']
+        expect = ["猫「にゃおにゃ。ただしかわいいものとする。異議は認める」。", "にゃお。", "にゃにゃ"]
         result = corpus.tokenize(DOCUMENT3)
         self.assertEqual(expect, result)
 
     def test_sentence_tokenize_with_combined(self):
         corpus = SentenceTokenizer()
-        expect = ['わんわん。', '「にゃ？」(にゃー）わんわん。', '「わおーん。」（犬より。）']
+        expect = ["わんわん。", "「にゃ？」(にゃー）わんわん。", "「わおーん。」（犬より。）"]
         result = corpus.tokenize(DOCUMENT4)
         self.assertEqual(expect, result)
