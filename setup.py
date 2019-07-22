7ccee042e748327da0cb77995dbd19f9e80b4733
himkt <himkt@klis.tsukuba.ac.jp>
2019-07-22 19:56:08 +0900

    Bumpup version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
 
 setup(
     name="tiny_tokenizer",
-    version="2.0.0",
+    version="2.1.0",
     description="Tiny Word/Sentence Tokenizer",
     author="himkt",
     author_email="himkt@klis.tsukuba.ac.jp",
