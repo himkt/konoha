8e4dbed75ca48785e8c8b0349dd38c71f44f70bf
himkt <himkt@klis.tsukuba.ac.jp>
2019-08-27 23:19:58 +0900

    Add attribute postag2 to Token

--- tiny_tokenizer/tiny_tokenizer_token.py
+++ tiny_tokenizer/tiny_tokenizer_token.py
@@ -2,9 +2,14 @@ from typing import Optional
 
 
 class Token:
-    """Token class."""
+    """Token class for tiny_tokenizer."""
 
-    def __init__(self, surface: str, postag: Optional[str] = None):
+    def __init__(
+            self,
+            surface: str,
+            postag: Optional[str] = None,
+            postag2: Optional[str] = None
+    ):
         """
         Initializer for Token.
 
@@ -12,11 +17,14 @@ class Token:
         ---
         surface (str)
             surface (original form) of a word
-        postag (Optional[str]=None)
-            part-of-speech tag of a word (option)
+        postag (Optional[str], default None)
+            part-of-speech tag of a word (optional)
+        postag2 (Optional[str], default None)
+            detailed part-of-speech tag of a word (optional)
         """
         self.surface = surface
-        self.postag = postag
+        self.postag  = postag  # NOQA
+        self.postag2 = postag2
 
     def __repr__(self):
         representation = self.surface
@@ -25,4 +33,6 @@ class Token:
         return representation
 
     def __eq__(self, right):
-        return self.surface == right.surface and self.postag == right.postag
+        return self.surface == right.surface \
+            and self.postag == right.postag \
+            and self.postag2 == right.postag2
