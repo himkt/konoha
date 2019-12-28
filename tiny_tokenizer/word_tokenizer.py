6770448a436cbd3964a487f3979e23cb11afa9c7
himkt <himkt@cookpad.com>
2019-12-28 14:37:35 +0900

    Add janome tokenizer

--- tiny_tokenizer/word_tokenizer.py
+++ tiny_tokenizer/word_tokenizer.py
@@ -35,26 +35,38 @@ class WordTokenizer:
     def __setup_tokenizer(self):
         if self._tokenizer == "character":
             self.tokenizer = word_tokenizers.CharacterTokenizer()
+
         if self._tokenizer == "whitespace":
             self.tokenizer = word_tokenizers.WhitespaceTokenizer()
+
         if self._tokenizer == "kytea":
             self.tokenizer = word_tokenizers.KyTeaTokenizer(
                 with_postag=self.with_postag, model_path=self.model_path
             )
+
         if self._tokenizer == "sentencepiece":
             self.tokenizer = word_tokenizers.SentencepieceTokenizer(
                 model_path=self.model_path
-            )  # NOQA
+            )
+
         if self._tokenizer == "mecab":
             self.tokenizer = word_tokenizers.MeCabTokenizer(
-                user_dictionary_path=self.user_dictionary_path,  # NOQA
-                system_dictionary_path=self.system_dictionary_path,  # NOQA
+                user_dictionary_path=self.user_dictionary_path,
+                system_dictionary_path=self.system_dictionary_path,
                 with_postag=self.with_postag,
-            )  # NOQA
+            )
+
+        if self._tokenizer == "janome":
+            self.tokenizer = word_tokenizers.JanomeTokenizer(
+                user_dictionary_path=self.user_dictionary_path,
+                with_postag=self.with_postag
+            )
+
         if self._tokenizer == "sudachi":
             self.tokenizer = word_tokenizers.SudachiTokenizer(
-                mode=self.mode, with_postag=self.with_postag
-            )  # NOQA
+                mode=self.mode,
+                with_postag=self.with_postag
+            )
 
     def tokenize(self, text: str):
         """Tokenize input text"""
@@ -76,6 +88,9 @@ if __name__ == "__main__":
     print(tokenizer.tokenize("我輩は猫である"))
 
     tokenizer = WordTokenizer("kytea", with_postag=True)
+    print(tokenizer.tokenize("我輩は猫である"))
+
+    tokenizer = WordTokenizer("janome", with_postag=True)
     print(tokenizer.tokenize("我輩は猫である"))
 
     tokenizer = WordTokenizer("sentencepiece", model_path="./data/model.spm")
