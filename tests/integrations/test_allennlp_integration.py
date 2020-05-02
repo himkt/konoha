6b818f0ba6d1f1224930fb4e82cf3ccd9a75e6b5
himkt <himkt@cookpad.com>
2020-05-02 22:20:08 +0900

    Skip test for integration if it is not available

--- tests/integrations/test_allennlp_integration.py
+++ tests/integrations/test_allennlp_integration.py
@@ -1,7 +1,15 @@
+import pytest
+
 from konoha.integrations.allennlp import KonohaTokenizer
 
 
 def test_konoha_tokenizer():
+    try:
+        import allennlp
+        del allennlp
+    except ImportError:
+        pytest.skip("AllenNLP is not installed.")
+
     tokenizer = KonohaTokenizer()
     tokens_konoha = tokenizer.tokenize("吾輩は猫である")
     token_surfaces = ["吾輩", "は", "猫", "で", "ある"]
