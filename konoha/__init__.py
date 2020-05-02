a461a60e8eb1d914ca6de9089ef865805b55b73e
himkt <himkt@cookpad.com>
2020-05-02 22:16:07 +0900

    Implement konoha integration for AllenNLP

--- konoha/__init__.py
+++ konoha/__init__.py
@@ -1,5 +1,6 @@
 """__init__.py."""
 from .sentence_tokenizer import SentenceTokenizer  # NOQA
 from .word_tokenizer import WordTokenizer  # NOQA
+from . import integrations  # NOQA
 
 __version__ = "4.0.0"
