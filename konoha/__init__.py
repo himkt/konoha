852973a87befc4ae4e2c870dbaa7926a9b882666
himkt <himkt@cookpad.com>
2020-05-03 06:05:09 +0900

    Stop exposing KonohaTokenizer under konoha

--- konoha/__init__.py
+++ konoha/__init__.py
@@ -1,6 +1,6 @@
 """__init__.py."""
 from konoha.sentence_tokenizer import SentenceTokenizer  # NOQA
 from konoha.word_tokenizer import WordTokenizer  # NOQA
-from konoha.integrations.allennlp import KonohaTokenizer  # NOQA
+
 
 __version__ = "4.0.0"
