6f5f40090bfe8b0ef42613ae92226882fc926e3c
himkt <himkt@cookpad.com>
2020-05-09 23:13:12 +0900

    Tweak code-fix

--- konoha/word_tokenizer.py
+++ konoha/word_tokenizer.py
@@ -1,4 +1,5 @@
 """Word Level Tokenizer."""
+from typing import Any
 from typing import List
 from typing import Optional
 
@@ -37,6 +38,7 @@ class WordTokenizer:
         self.model_path = model.path
         self.mode = mode.lower() if mode is not None else None
         self.dictionary_format = dictionary_format
+        self.tokenizer = None  # type: Any
 
         self._setup_tokenizer()
 
@@ -50,12 +52,12 @@ class WordTokenizer:
         if self._tokenizer == "kytea":
             self.tokenizer = word_tokenizers.KyTeaTokenizer(
                 with_postag=self.with_postag,
-                model_path=self.model_path
+                model_path=self.model_path,
             )
 
         if self._tokenizer == "sentencepiece":
             self.tokenizer = word_tokenizers.SentencepieceTokenizer(
-                model_path=self.model_path
+                model_path=self.model_path,
             )
 
         if self._tokenizer == "mecab":
@@ -69,13 +71,13 @@ class WordTokenizer:
         if self._tokenizer == "janome":
             self.tokenizer = word_tokenizers.JanomeTokenizer(
                 user_dictionary_path=self.user_dictionary_path,
-                with_postag=self.with_postag
+                with_postag=self.with_postag,
             )
 
         if self._tokenizer == "sudachi":
             self.tokenizer = word_tokenizers.SudachiTokenizer(
                 mode=self.mode,
-                with_postag=self.with_postag
+                with_postag=self.with_postag,
             )
 
     def tokenize(self, text: str) -> List[Token]:
