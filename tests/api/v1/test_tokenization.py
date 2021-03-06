c2398d51d1d84105577a5b0a5747bc18c8ca8106
Makoto Hiramatsu <himkt@cookpad.com>
2021-03-06 23:46:10 +0900

    Add request tests

---
+++ tests/api/v1/test_tokenization.py
@@ -0,0 +1,31 @@
+from typing import Dict
+from fastapi.testclient import TestClient
+
+import pytest
+
+from konoha.api.server import create_app
+
+
+app = create_app()
+client = TestClient(app)
+
+
+@pytest.mark.parametrize(
+    "tokenizer_params", [
+        {"tokenizer": "mecab"},
+        {"tokenizer": "sudachi", "mode": "A"},
+        {"tokenizer": "sudachi", "mode": "B"},
+        {"tokenizer": "sudachi", "mode": "C"},
+        {"tokenizer": "sentencepiece", "model_path": "data/model.knm"},
+        {"tokenizer": "kytea", "model_path": "data/model.knm"},
+        {"tokenizer": "character"},
+        {"tokenizer": "nagisa"},
+        {"tokenizer": "janome"},
+    ]
+)
+def test_tokenization(tokenizer_params: Dict):
+    headers = {"Content-Type": "application/json"}
+    params = dict(tokenizer_params, text="私は猫")
+    response = client.post("/api/v1/tokenize", headers=headers, json=params)
+    assert response.status_code == 200
+    assert "tokens" in response.json()
