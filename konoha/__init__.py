7696d5ffeb43966c8c36c310e66d9923af03b676
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2021-03-06 20:09:21 +0900

    Packaging/poetry dynamic versioning (#118)
    * Introduce poetry-dynamic-versioning and importlib_metadata
    
    * Run poetry update
    
    * Avoid hard-coded version definition

--- konoha/__init__.py
+++ konoha/__init__.py
@@ -1,5 +1,7 @@
 """__init__.py."""
 from konoha.sentence_tokenizer import SentenceTokenizer  # NOQA
 from konoha.word_tokenizer import WordTokenizer  # NOQA
+from importlib_metadata import version
 
-__version__ = "4.6.3"
+
+__version__ = version("konoha")
