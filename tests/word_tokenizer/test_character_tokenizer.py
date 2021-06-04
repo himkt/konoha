103ea4520aeeba4689395f481c31886e4603ced1
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2021-06-04 22:47:12 +0900

    Update tests (#136)
    * Update test data
    
    * Update tests
    
    * Unify workflows

--- tests/word_tokenizer/test_character_tokenizer.py
+++
@@ -1,9 +0,0 @@
-from konoha.konoha_token import Token
-from konoha.word_tokenizer import WordTokenizer
-
-
-def test_word_tokenize_with_character():
-    tokenizer = WordTokenizer(tokenizer="Character")
-    expect = [Token(surface=w) for w in "吾 輩 は 猫 で あ る".split(" ")]
-    result = tokenizer.tokenize("吾輩は猫である")
-    assert expect == result
--- tests/word_tokenizer/test_word_tokenizer.py
+++
@@ -1,7 +0,0 @@
-from konoha.word_tokenizer import WordTokenizer
-
-
-def test_word_tokenizer():
-    tokenizer1 = WordTokenizer(tokenizer="Whitespace")
-    tokenizer2 = WordTokenizer(tokenizer="whitespace")
-    assert tokenizer1.tokenizer.name == tokenizer2.tokenizer.name
