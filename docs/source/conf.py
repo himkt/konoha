4604a2a5dfbdc630fa2aad203df896003f41263d
himkt <himkt@klis.tsukuba.ac.jp>
2022-09-10 08:47:40 +0900

    Upgrade sphinx version and change theme (from `sphinx_rtd` to `pydata`) (#169)
    * Introduce pydata-sphinx-theme
    
    * Change html-theme for the docs from rtd to pydata

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 if not on_rtd:
-    html_theme = 'sphinx_rtd_theme'
+    html_theme = 'pydata_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
