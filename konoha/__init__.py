1899c8245a784ba02e84cddcf16165bcef0f9887
himkt <himkt@klis.tsukuba.ac.jp>
2024-01-12 09:12:21 +0900

    deps: remove importlib-metadata since konoha now for py38 or newer

--- konoha/__init__.py
+++ konoha/__init__.py
@@ -1,5 +1,5 @@
 """__init__.py."""
-from importlib_metadata import version
+from importlib.metadata import version
 
 from konoha.sentence_tokenizer import SentenceTokenizer  # NOQA
 from konoha.word_tokenizer import WordTokenizer  # NOQA
